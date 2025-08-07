9845293dcba1a56ef79094619e92a57364c6a47b
Yaroslav-Hoichuk <qwerty1234tq@gmail.com>
2025-08-07 13:19:49 +0300

    initialization of the project

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express"; 
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 8989;
+
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.oceaqdv.mongodb.net/")
+  .then(() => console.log("BD is available"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
